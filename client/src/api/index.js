1c40d675273346fb440b3265ecae367489387d54
le000222 <erile200105@gmail.com>
2025-01-09 16:09:39 -0500

    Update api to deployed Vercel API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API = axios.create({ baseURL: "https://mern-shop-lysa.vercel.app/" });
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "https://mern-shop-lysa.vercel.app/" });
+// const API = axios.create({ baseURL: "http://localhost:5000/" });
 
 const isTokenExpired = () => {
 	const expiration = localStorage.getItem("tokenExpiration");
